0d59b768107234a30c33b908f0700de6516f31cb
Dingus <devindperez@gmail.com>
2019-08-12 15:35:41 -0700

    added mail management

---
+++ code/modules/mail/manifests/init.pp
@@ -0,0 +1,14 @@
+class mail {
+    package { "mailutils":
+        ensure => installed;
+    }
+
+    service { "postfix",
+        enable => true,
+        ensure => running,
+    }
+
+
+
+
+}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,13 @@
-node ip-10-0-2-219 {
+node ip-52-12-236-183 {
     include puppet
     include sshd
     include apache2
+    include mail
 }
 
-node ip-10-0-2-157 {
+node ip-34-214-109-239{
     include puppet
     include sshd
     include apache2
+    inlcude mail
 }
