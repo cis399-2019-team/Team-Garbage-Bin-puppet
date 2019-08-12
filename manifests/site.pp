6ab557d17d91deb561259a57add0b12ffddd9f8f
Dingus <devindperez@gmail.com>
2019-08-12 15:44:50 -0700

    debugging

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,10 @@ node ip-52-12-236-183 {
     include puppet
     include sshd
     include apache2
-    include mail
 }
 
 node ip-34-214-109-239 {
     include puppet
     include sshd
     include apache2
-    inlcude mail
 }
